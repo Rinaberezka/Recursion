92f36df6ed861391678b96b74b90f6815b57c1d2
Iryna Brezovskaya <Rinaberezka@gmail.com>
2024-03-20 15:41:01 +0300

    Сделала репозиторий для task01

---
+++ task01/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая
+// выведет все натуральные числа в промежутке от M до N.
+// Использовать рекурсию, не использовать циклы.
+
+void FuncNutNumber(int m, int n)
+{
+    if (n == m)
+    {
+        return;
+    }
+    if (m < n)
+    {
+        Console.WriteLine(m);
+        m++;
+        FuncNutNumber(m, n);
+    }
+}
+
+Console.WriteLine("Введите значение M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+FuncNutNumber(m, n+1);
